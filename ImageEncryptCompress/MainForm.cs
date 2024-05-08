34dfc30dfaad6d5dcc803df6f2cb90a1110428a1
joonyou34 <yaheahaha@gmail.com>
2024-05-09 00:30:15 +0300

    Add project files.

---
+++ ImageEncryptCompress/MainForm.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ImageEncryptCompress
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        RGBPixel[,] ImageMatrix;
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Open the browsed image and display it
+                string OpenedFilePath = openFileDialog1.FileName;
+                ImageMatrix = ImageOperations.OpenImage(OpenedFilePath);
+                ImageOperations.DisplayImage(ImageMatrix, pictureBox1);
+            }
+            txtWidth.Text = ImageOperations.GetWidth(ImageMatrix).ToString();
+            txtHeight.Text = ImageOperations.GetHeight(ImageMatrix).ToString();
+        }
+
+        private void btnGaussSmooth_Click(object sender, EventArgs e)
+        {
+            double sigma = double.Parse(txtGaussSigma.Text);
+            int maskSize = (int)nudMaskSize.Value ;
+            ImageMatrix = ImageOperations.GaussianFilter1D(ImageMatrix, maskSize, sigma);
+            ImageOperations.DisplayImage(ImageMatrix, pictureBox2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string binString = binaryString.Text;
+            BitArray seed = new BitArray(binString.Length);
+            for(int i = 0; i < binString.Length; i++)
+            {
+                seed[i] = (binString[i] == '1' ? true : false);
+            }
+            int tap = ((int)tapAmount.Value);
+            ImageMatrix = ImageOperations.EncryptImage(ImageMatrix, binString.Length, seed, tap);
+            ImageOperations.DisplayImage(ImageMatrix, pictureBox2);
+        }
+    }
+}
